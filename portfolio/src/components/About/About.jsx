a8595d4755070fc0b90832e3f29b38b684c77c6d
arthur <arthurfantasma02@gmail.com>
2026-07-29 19:24:09 -0300

    PErfil e About atualizados

--- portfolio/src/components/About/About.jsx
+++ portfolio/src/components/About/About.jsx
@@ -1,3 +1,4 @@
+// About.jsx
 import "./About.css";
 
 function About() {
@@ -5,25 +6,32 @@ function About() {
         <section id="about">
             <h2>About Me</h2>
 
-            <p>
-                I started programming because I wanted to build things that solve real problems...
-            </p>
+            <div className="about-content">
+                <div className="about-text">
+                    <p>
+                        I started programming because I wanted to build things that solve real problems...
+                    </p>
 
-            <p>
-                I enjoy learning how things work behind the scenes...
-            </p>
+                    <p>
+                        I enjoy learning how things work behind the scenes...
+                    </p>
 
-            <p>
-                Today I'm looking for my first opportunity...
-            </p>
+                    <p>
+                        Today I'm looking for my first opportunity...
+                    </p>
 
-            <blockquote>
-                "I believe good software isn't about writing more code.
-                It's about solving the right problems."
-            </blockquote>
+                    <blockquote>
+                        "I believe good software isn't about writing more code.
+                        It's about solving the right problems."
+                    </blockquote>
+                </div>
+
+                <div className="about-photo">
+                    <img src="./Perfil/Perfil.jpg" alt="Foto de Arthur Messias" />
+                </div>
+            </div>
         </section>
-    )
+    );
 }
-
 
 export default About;
